e61b69070fcedfffdce8bb9ac4bd1881ab6015db
fomentia <isaac@thewilliams.ws>
2017-03-21 16:11:26 -0400

    Maintain local database to track scraping progress and S3 URLs
    Still need to actually upload files to S3 though.

--- wfth.rb
+++ wfth.rb
@@ -1,76 +1,97 @@
 require 'pp'
 require 'rubygems'
 require 'mechanize'
+require 'sqlite3'
 require 'json'
 
-# TODO
-# - Report progress better
-# - Create better sermon metadata (null passages, not full passages)
+# TODO: report progress
 
 $agent = Mechanize.new
 
 def main
-  puts "Loading all messages...\n"
+  db = SQLite3::Database.new("wfth.db")
+  create_db_tables(db)
+
   messages_page = $agent.get("http://www.wisdomonline.org/media/messages")
   scripture_links = messages_page.search("div#scripture li a")
-  
+
   for scripture in scripture_links
-    puts "Loading #{scripture_links.index(scripture) + 1} scripture..."
     scripture_page = $agent.click(scripture)
 
     for series in scripture_page.search(".series_list > li")
-      series_base_path = "/tmp/WFTH/#{scripture.text}/#{series.search(".title").text}"
-      FileUtils.mkdir_p series_base_path
-      
-      puts "Compiling series metadata..."
-      compile_series_metadata(series, series_base_path)
+      series_metadata = compile_series_metadata(series)
 
-      puts "Downloading series graphic..."
-      download_graphic(series, series_base_path)
-      
+      db.execute("insert into sermon_series (title, description, released_on, graphic_url) values (?, ?, ?, ?)",
+                 series_metadata["title"],
+                 series_metadata["description"],
+                 series_metadata["date"],
+                 "nothing right now")
+      series_id = `sqlite3 wfth.db "select series_id from sermon_series order by series_id desc limit 1;"`
+
       for sermon in series.search(".series_links > ul > li")
-        puts "Extracting " + sermon.search(".sermon_title").text + "..."
-        extract_data(scripture, series, sermon, series_base_path)
+        sermon_metadata = compile_sermon_metadata(sermon)
+        db.execute("insert into sermons (title, passage, sermon_series_id, audio_url, transcript_url) values (?, ?, ?, ?, ?)",
+                    sermon_metadata["title"],
+                    sermon_metadata["passage"],
+                    series_id,
+                    "nothing right now",
+                    "nothing right now")
       end
     end
   end
 end
 
-def extract_data(scripture, series, sermon, base_path)
-  puts "Downloading transcript..."
-  download_transcript(sermon, base_path + "/#{sermon.search(".sermon_title").text}")
+def create_db_tables(db)
+  db.execute <<-SQL
+    create table if not exists sermon_series (
+      series_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+      title TEXT NOT NULL,
+      description TEXT,
+      released_on TEXT,
+      graphic_url TEXT
+    );
+  SQL
 
-  puts "Compiling sermon metadata..."
-  compile_sermon_metadata(sermon, base_path + "/#{sermon.search(".sermon_title").text}")
-
-  puts "Downloading sermon audio..."
-  download_audio(sermon, base_path + "/#{sermon.search(".sermon_title").text}")
-
-  puts "Finished!"
+  db.execute <<-SQL
+    create table if not exists sermons (
+      sermon_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+      title TEXT NOT NULL,
+      passage TEXT,
+      sermon_series_id INTEGER NOT NULL,
+      audio_url TEXT,
+      transcript_url TEXT
+    )
+  SQL
 end
 
-def download_transcript(sermon, path)
-  if sermon.search(".transcript a")[0]
-    transcript = $agent.click(sermon.search(".transcript a")[0])
-    transcript.save(path + "/Transcript.pdf")
-  end
-end
+# TODO: upload transcript to S3 and return url
 
-def download_audio(sermon, path)
-  if sermon.search(".audio a")[0]
-    audio = $agent.click(sermon.search(".audio a")[0])
-    audio.save(path + "/Audio.mp3")
-  end
-end
+# def download_transcript(sermon, path)
+#   if sermon.search(".transcript a")[0]
+#     transcript = $agent.click(sermon.search(".transcript a")[0])
+#     transcript.save(path + "/Transcript.pdf")
+#   end
+# end
 
-def download_graphic(series, path)
-  graphic = series.search(".series_graphic img")[0]
-  if graphic
-    $agent.get(graphic.attribute("src")).save(path + "/Graphic.jpg")
-  end
-end
+# TODO: upload audio to S3 and return url
 
-def compile_series_metadata(series, path)
+# def download_audio(sermon, path)
+#   if sermon.search(".audio a")[0]
+#     audio = $agent.click(sermon.search(".audio a")[0])
+#     audio.save(path + "/Audio.mp3")
+#   end
+# end
+
+# TODO: upload graphic to S3 and return url
+
+# def download_graphic(series, path)
+#   graphic = series.search(".series_graphic img")[0]
+#   if graphic
+#     $agent.get(graphic.attribute("src")).save(path + "/Graphic.jpg")
+#   end
+# end
+
+def compile_series_metadata(series)
   metadata = {
     "title" => series.search(".title").text,
     "date" => series.search(".date").text,
@@ -81,36 +102,25 @@ def compile_series_metadata(series, path)
     metadata["buy_link"] = series.search(".link-buy-series")[0].attributes["href"].text
   end
 
-  File.open(path + "/metadata.json", "w") do |f|
-    f.write(metadata.to_json)
-  end
+  return metadata
 end
 
-def compile_sermon_metadata(sermon, path)
+def compile_sermon_metadata(sermon)
+  sermon_title = sermon.search(".sermon_title").text[/.+?(?= -)/]
+  if sermon_title == nil
+    sermon_title = sermon.search(".sermon_title").text[/.+/]
+  end
+
   metadata = {
-    "passage" => sermon.search(".sermon_title").text[/\w+ \d{1,3}:\d{1,3}/] # doesn't work with Genesis 1:1-29 or other "through" passages
+    "title" => sermon_title,
+    "passage" => sermon.search(".sermon_title").text[/(?<=- ).+/]
   }
 
   if sermon.search(".buy_single a")[0]
     metadata["buy_link"] = sermon.search(".buy_single a").attribute("href").text
   end
 
-  File.open(path + "/metadata.json", "w") do |f|
-    f.write(metadata.to_json)
-  end
-end
-
-def number_ending(num)
-  case num % 10
-  when 1
-    "st"
-  when 2
-    "nd"
-  when 3
-    "rd"
-  else
-    "th"
-  end
+  return metadata
 end
 
 main()
