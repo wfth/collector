58c58e1e0ba90adc726e2d9c343e6b72166b9fd8
fomentia <isaac@thewilliams.ws>
2017-03-27 15:20:43 -0400

    Clean up main by encapsulating implementation

--- wfth.rb
+++ wfth.rb
@@ -6,13 +6,11 @@ require 'json'
 require 'aws-sdk'
 require 'fileutils'
 
-# TODO: report progress
-
 $agent = Mechanize.new
 
 def main
   setup_aws
-  db = create_db
+  create_db
 
   messages_page = $agent.get("http://www.wisdomonline.org/media/messages")
   scripture_links = messages_page.search("div#scripture li a")
@@ -21,31 +19,10 @@ def main
     scripture_page = $agent.click(scripture)
 
     for series in scripture_page.search(".series_list > li")
-      series_metadata = compile_series_metadata(series)
-
-      db.execute("insert into sermon_series (title, description, released_on) values (?, ?, ?)",
-                 series_metadata["title"],
-                 series_metadata["description"],
-                 series_metadata["date"])
-      series_id = `sqlite3 wfth.db "select series_id from sermon_series order by series_id desc limit 1;"`.to_i
-
-      graphic_key = upload_graphic("series/#{series_id}/graphic.jpg", series)
-      db.execute("update sermon_series set graphic_key = '#{graphic_key}' where series_id = #{series_id}")
+      series_id = insert_series(series)
 
       for sermon in series.search(".series_links > ul > li")
-        sermon_metadata = compile_sermon_metadata(sermon)
-
-        db.execute("insert into sermons (title, passage, sermon_series_id) values (?, ?, ?)",
-                    sermon_metadata["title"],
-                    sermon_metadata["passage"],
-                    series_id)
-        sermon_id = `sqlite3 wfth.db "select sermon_id from sermons order by sermon_id desc limit 1;"`.to_i
-
-        transcript_key = upload_transcript("series/#{series_id}/sermons/#{sermon_id}/transcript.pdf", sermon)
-        db.execute("update sermons set transcript_key = '#{transcript_key}' where sermon_id is #{sermon_id}")
-
-        audio_key = upload_audio("series/#{series_id}/sermons/#{sermon_id}/audio.mp3", sermon)
-        db.execute("update sermons set audio_key = '#{audio_key}' where sermon_id is #{sermon_id}")
+        insert_sermon(sermon, series_id)
       end
     end
   end
@@ -55,19 +32,11 @@ def setup_aws
   Aws.config[:credentials] = Aws::Credentials.new(ENV['WFTH_PERMISSIONS_AWS_ACCESS_KEY'], ENV['WFTH_PERMISSIONS_AWS_ACCESS_SECRET'])
 end
 
-def upload_file(object_key, file_path)
-  s3 = Aws::S3::Resource.new(region: 'us-east-1')
-  obj = s3.bucket('wisdomonline-development').object(object_key)
-  obj.upload_file(file_path)
-
-  return obj.key
-end
-
 def create_db
   `> wfth.db` # Wipe wfth.db of all data, if it exists
-  db = SQLite3::Database.new("wfth.db")
+  $db = SQLite3::Database.new("wfth.db")
 
-  db.execute <<-SQL
+  $db.execute <<-SQL
     create table sermon_series (
       series_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
       title TEXT NOT NULL,
@@ -77,7 +46,7 @@ def create_db
     );
   SQL
 
-  db.execute <<-SQL
+  $db.execute <<-SQL
     create table sermons (
       sermon_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
       title TEXT NOT NULL,
@@ -87,8 +56,61 @@ def create_db
       transcript_key TEXT
     );
   SQL
+end
 
-  return db
+def insert_series(series)
+  series_metadata = compile_series_metadata(series)
+
+  $db.execute("insert into sermon_series (title, description, released_on) values (?, ?, ?)",
+             series_metadata["title"],
+             series_metadata["description"],
+             series_metadata["date"])
+  series_id = `sqlite3 wfth.db "select series_id from sermon_series order by series_id desc limit 1;"`.to_i
+
+  graphic_key = upload_graphic("series/#{series_id}/graphic.jpg", series)
+  $db.execute("update sermon_series set graphic_key = '#{graphic_key}' where series_id = #{series_id}")
+
+  return series_id
+end
+
+def insert_sermon(sermon, series_id)
+  sermon_metadata = compile_sermon_metadata(sermon)
+
+  $db.execute("insert into sermons (title, passage, sermon_series_id) values (?, ?, ?)",
+             sermon_metadata["title"],
+             sermon_metadata["passage"],
+             series_id)
+  sermon_id = `sqlite3 wfth.db "select sermon_id from sermons order by sermon_id desc limit 1;"`.to_i
+
+  transcript_key = upload_transcript("series/#{series_id}/sermons/#{sermon_id}/transcript.pdf", sermon)
+  $db.execute("update sermons set transcript_key = '#{transcript_key}' where sermon_id is #{sermon_id}")
+
+  audio_key = upload_audio("series/#{series_id}/sermons/#{sermon_id}/audio.mp3", sermon)
+  $db.execute("update sermons set audio_key = '#{audio_key}' where sermon_id is #{sermon_id}")
+end
+
+def compile_series_metadata(series)
+  metadata = {
+    "title" => series.search(".title").text,
+    "date" => series.search(".date").text,
+    "description" => series.search(".description p").text
+  }
+
+  return metadata
+end
+
+def compile_sermon_metadata(sermon)
+  sermon_title = sermon.search(".sermon_title").text[/.+?(?= -)/]
+  if sermon_title == nil
+    sermon_title = sermon.search(".sermon_title").text[/.+/]
+  end
+
+  metadata = {
+    "title" => sermon_title,
+    "passage" => sermon.search(".sermon_title").text[/(?<=- ).+/]
+  }
+
+  return metadata
 end
 
 def upload_transcript(object_key, sermon)
@@ -125,28 +147,12 @@ def upload_graphic(object_key, series)
   end
 end
 
-def compile_series_metadata(series)
-  metadata = {
-    "title" => series.search(".title").text,
-    "date" => series.search(".date").text,
-    "description" => series.search(".description p").text
-  }
+def upload_file(object_key, file_path)
+  s3 = Aws::S3::Resource.new(region: 'us-east-1')
+  obj = s3.bucket('wisdomonline-development').object(object_key)
+  obj.upload_file(file_path)
 
-  return metadata
-end
-
-def compile_sermon_metadata(sermon)
-  sermon_title = sermon.search(".sermon_title").text[/.+?(?= -)/]
-  if sermon_title == nil
-    sermon_title = sermon.search(".sermon_title").text[/.+/]
-  end
-
-  metadata = {
-    "title" => sermon_title,
-    "passage" => sermon.search(".sermon_title").text[/(?<=- ).+/]
-  }
-
-  return metadata
+  return obj.key
 end
 
 main()
