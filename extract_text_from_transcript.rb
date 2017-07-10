a51c8c610c42e2be376d926892ef8e572720b644
ijwilliams <isaac@thewilliams.ws>
2017-07-10 07:14:22 -0400

    Minimally format extracted text from transcript PDFs
    Advances #12

--- extract_text_from_transcript.rb
+++ extract_text_from_transcript.rb
@@ -16,11 +16,34 @@ def main(pdf_doc)
   transcript = Nokogiri::XML(transcript_xml)
   texts = transcript.css("text")
   plain_text = ""
+
+  last_broke = false
   for t in texts
-    plain_text = plain_text + " " + t.text
+    if t.attribute("width").value.to_i < 15 || t.text.length < 2
+      next
+    end
+
+    new_text = t.text
+
+    if t.attribute("height").value.to_i > 15
+      new_text = "<h4>" + new_text + "</h4>"
+      plain_text = plain_text + new_text
+      last_broke = true
+      next
+    end
+
+    if t.attribute("width").value.to_i < 240 && last_broke != true
+      plain_text = plain_text + " " + new_text + "</p>\n\n"
+      last_broke = true
+    elsif last_broke
+      plain_text = plain_text + "<p>" + new_text
+      last_broke = false
+    else
+      plain_text = plain_text + " " + new_text
+    end
   end
 
-  puts plain_text
+  puts plain_text + "</p>"
 end
 
 params = parse_params
