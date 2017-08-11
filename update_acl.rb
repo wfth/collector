6df4bd40dfa49eb499d363e72f4d97c85d0701d9
Adam Williams <adam@thewilliams.ws>
2017-08-11 17:31:00 -0400

    Simplify ACL update script
    All we need is the table and the column that has the S3 key.

--- update_acl.rb
+++ update_acl.rb
@@ -1,60 +1,30 @@
-$LOAD_PATH.unshift(".")
+ENV['AWS_PROFILE'] = 'wfth'
 
 require 'pg'
 require 'aws-sdk'
-require 'beachball'
 
-def main
-  print "Updating sermon series ACLs "
-  series_beachball = Beachball.new(10)
-  series_beachball.start
-
-  db.exec("select graphic_key, buy_graphic_key from sermon_series").each do |row|
-    make_public_readable(row["graphic_key"], row["buy_graphic_key"])
+def main(table, column)
+  db.exec("select id, #{column} from #{table}").each do |row|
+    obj = s3_object(row[column])
+    if obj && obj.exists?
+      puts "Updating ACL: #{table}.#{column}##{row["id"]} #{row[column]}"
+      obj.acl.put({acl: "public-read"})
+    else
+      puts "! Missing: #{table}.#{column}##{row["id"]} #{row[column]}"
+    end
   end
-
-  print "\b"
-  series_beachball.stop
-  puts "\nFinished sermon series."
-
-  print "Updating sermon ACLs "
-  sermon_beachball = Beachball.new(10)
-  sermon_beachball.start
-
-  db.exec("select audio_key, buy_graphic_key from sermons").each do |row|
-    make_public_readable(row["audio_key"], row["buy_graphic_key"])
-  end
-
-  sermon_beachball.stop
 end
 
 def s3
-  return @s3 ||= Aws::S3::Resource.new(region: 'us-east-1')
+  @s3 ||= Aws::S3::Resource.new(region: 'us-east-1')
 end
 
 def s3_object(key)
   s3.bucket('wisdomonline-development').object(key)
 end
 
-def make_public_readable(*keys)
-  keys.each do |key|
-    s3_object(key).acl.put({acl: "public-read"}) if s3_object(key)
-  end
-end
-
 def db
-  unless @db
-    initial_connection = PG.connect(user: "postgres", password: "postgres")
-
-    begin
-      @db = PG.connect(dbname: 'collector_dev', user: "postgres", password: "postgres")
-    rescue
-      puts "Please make sure the collector_dev Postgres database exists!"
-      exit
-    end
-  end
-
-  return @db
+  @db ||= PG.connect(dbname: "collector_dev", user: "postgres", password: "postgres")
 end
 
-main()
+main(ARGV[0], ARGV[1])
