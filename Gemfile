426f5efaaca2439dc04cd7cd41192eeb88df02cd
Adam Williams <adam@thewilliams.ws>
2017-08-11 16:36:40 -0400

    Remove shopify codes

--- Gemfile
+++ Gemfile
@@ -4,5 +4,4 @@ source "https://rubygems.org"
 gem 'mechanize'
 gem 'aws-sdk', '~> 2'
 gem 'beachball'
-gem 'shopify_api'
 gem 'pg'
--- shopify.rb
+++
@@ -1,4 +0,0 @@
-require 'shopify_api'
-
-shop_url = "https://#{WFTH_PERMISSIONS_SHOPIFY_COLLECTOR_API_KEY}:#{WFTH_PERMISSIONS_SHOPIFY_COLLECTOR_PASSWORD}@#{WFTH_SHOPIFY_SHOP_NAME}.myshopify.com/admin"
-ShopifyAPI::Base.site = shop_url
